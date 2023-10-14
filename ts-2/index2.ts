f287487867270b897731b1c0747a99100048b76b
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-14 19:14:35 +0600

    TypeScript - Lesoon 1-1

--- ts-2/index2.ts
+++ ts-2/index2.ts
@@ -2,12 +2,24 @@ const isBirthdayData: boolean = true;
 let ageData: number = 29;
 const userNameData: string = "Daurenbek Shamidinov";
 
-function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
+// function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
+//   if (isBirthday) {
+//     return `Congrats ${userName.toUpperCase()}, age ${age + 1}`;
+//   } else {
+//     return "Error";
+//   }
+// }
+
+const logBrtMsg = (
+  isBirthday: boolean,
+  userName: string,
+  age: number
+): string => {
   if (isBirthday) {
     return `Congrats ${userName.toUpperCase()}, age ${age + 1}`;
   } else {
     return "Error";
   }
-}
+};
 
 logBrtMsg(isBirthdayData, userNameData, ageData);
