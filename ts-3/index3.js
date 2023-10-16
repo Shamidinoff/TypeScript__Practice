f50821f529370e2f431ea81ca5581ed0608ebdbe
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-16 20:21:44 +0600

    TypeScript - Lesoon 3

--- ts-3/index3.js
+++ ts-3/index3.js
@@ -1,12 +1,27 @@
-// let userName: string = "Daurenbek";
-// userName.isInteger(); - mistake because string is not a number
-// userName = 5; - you can't change let because string is not a number
-// '', "", `` - for the string type
-// 10, 0.5, 0.0001, -50, 4e10 - for the number type
-// true, false - for he boolean type
-var isBirthday = true;
-var age = 29;
-var userName = "Daurenbek Shamidinov";
-if (isBirthday) {
-    console.log("Congrats ".concat(userName.toUpperCase(), ", age ").concat(age + 1));
+// const isBirthdayData: boolean = true;
+// let ageData: number = 29;
+// const userNameData: string = "Daurenbek Shamidinov";
+var salary; // если не задать тип вручную то тип будет any и в будущем появиться ошибка, поэтому строго нужно задать соответствующий
+salary = 5000; // сюда уже нельзя писать текст, тип числовой
+var userData = '{"isBirthdayData": true, "ageData": 29, "userNameData": "Daurenbek Shamidinov"}';
+var userObj = JSON.parse(userData);
+function logBrtMsg(isBirthday, userName, age) {
+    if (isBirthday) {
+        return "Congrats ".concat(userName.toUpperCase(), ", age ").concat(age + 1);
+    }
+    else {
+        return "Error";
+    }
 }
+logBrtMsg(isBirthdayData, userNameData, ageData);
+// const logBrtMsg = (
+//   isBirthday: boolean,
+//   userName: string,
+//   age: number
+// ): string => {
+//   if (isBirthday) {
+//     return `Congrats ${userName.toUpperCase()}, age ${age + 1}`;
+//   } else {
+//     return "Error";
+//   }
+// };
