c8fbdbeefef7969698eda5c3cf4861b3e7b93301
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-24 20:24:20 +0600

    TypeScript - Lesoon 3

--- ts-14/index14.js
+++ ts-14/index14.js
@@ -1,7 +1,36 @@
 var msg = "Hello";
 msg = "Hello";
+var port3000 = 3000;
+var port3001 = 3001;
 function startServer(protocol, port) {
-    console.log("Server started on ".concat(protocol, "://server:").concat(port));
+    if (port === port3000 || port === port3001) {
+        console.log("Server started on ".concat(protocol, "://server:").concat(port));
+    }
+    else {
+        console.error("Invalid port");
+    }
     return "Server started";
 }
 startServer("https", 3001);
+function createAnimation(id, animNme, timingFunc, duration, iterCount) {
+    if (timingFunc === void 0) { timingFunc = 'ease'; }
+    var elem = document.querySelector("".concat(), { id: id });
+}
+") as HTMLElement;\n\n  if (elem) {\n\n    elem.style.animation = ";
+$;
+{
+    animNme;
+}
+$;
+{
+    timingFunc;
+}
+$;
+{
+    duration;
+}
+$;
+{
+    iterCount;
+}
+";\n  }\n}\n\ncreateAnimation('id', 'fade', 'ease-in', 5, 'infinite');\n";
